141066b886a0d828d7d278f2e117b526928e26e6
Roman Boiarchuk <r.s.boyarchuk@gmail.com>
2020-02-28 14:31:01 +0200

    generate spring webflux project

---
+++ postex-api/src/main/java/com/project/postexapi/PostexApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.postexapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostexApiApplication.class, args);
+	}
+
+}
