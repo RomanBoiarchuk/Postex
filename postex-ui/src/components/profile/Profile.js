67f4448c9b716b2a397dbfe36b51160086f268aa
Roman Boiarchuk <r.s.boyarchuk@gmail.com>
2020-03-19 19:29:51 +0200

    add subscription/unsubscription

--- postex-ui/src/components/profile/Profile.js
+++ postex-ui/src/components/profile/Profile.js
@@ -1,22 +1,40 @@
 import {Button, Card, Image} from "react-bootstrap";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
-import {authenticationService} from "../../services";
+import {accountService, authenticationService} from "../../services";
 
 export function Profile(props) {
     const account = props.account;
-    const isFriend = props.isFriend || false;
     let signUpDate = new Date(account.signUpDate);
     let month = new Intl.DateTimeFormat('en', {month: 'long'}).format(signUpDate);
     let year = new Intl.DateTimeFormat('en', {year: 'numeric'}).format(signUpDate);
     let dateStr = `${month} ${year}`;
 
+    const [isFriend, setIsFriend] = useState(props.isFriend || false);
+
+    useEffect(() => setIsFriend(props.isFriend || false), [props]);
+
+    const addFriend = () => {
+        accountService.addFriend(account.id)
+            .then(() => {
+                setIsFriend(true);
+            });
+    };
+
+    const removeFriend = () => {
+        accountService.removeFriend(account.id)
+            .then(() => {
+                setIsFriend(false);
+            });
+    };
+
     const actionButton = () => {
         if (authenticationService.account.id === account.id) {
-            return <Button style={{float: 'right'}} variant="primary">Edit</Button>;
+            return <Button className="float-right" variant="primary">Edit</Button>;
         }
-        return isFriend ? <Button style={{float: 'right'}} variant="primary">Unsubscribe</Button>
-            : <Button style={{float: 'right'}} variant="primary">Subscribe</Button>
+        return isFriend
+            ? <Button className="float-right" onClick={removeFriend} variant="primary">Unsubscribe</Button>
+            : <Button className="float-right" onClick={addFriend} variant="primary">Subscribe</Button>
     };
 
     return (
@@ -43,7 +61,7 @@ export function Profile(props) {
                     </div>
                     <Card.Title className='mb-0'>{`${account.firstName} ${account.lastName}`}</Card.Title>
                     <Card.Text className='mb-0'>{account.user.username}</Card.Text>
-                    <Card.Text className='mb-0'>Joined { dateStr}</Card.Text>
+                    <Card.Text className='mb-0'>Joined {dateStr}</Card.Text>
                     <Card.Text className='mb-0'>
                         <Link to={`/profile/${account.id}/friends`}>{account.friendsCount} Following</Link>
                         <Link className='ml-3'
